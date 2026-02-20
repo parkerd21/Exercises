9c922feeeac53e76900545f3331e092ac1b45ee1
Parker Downing <parkeraydowning@gmail.com>
2026-02-19 19:31:47 -0700

    Adds some array exercises and tests

---
+++ src/main/java/Arrayz.java
@@ -0,0 +1,290 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class Arrayz {
+
+  // optimal 
+  // O(n)
+  public static int largestElementInArray(int[] arr)
+  {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++)
+    {
+      if (arr[i] > max)
+      {
+        max = arr[i];
+      }
+    }
+
+    return max;
+  }
+
+  // brute force
+  // O(nlogn)
+  public static int bruteforceLargestElementInArray(int[] arr)
+  {
+    Arrays.sort(arr);
+    return arr[arr.length -1];
+  }
+
+
+  public static int[] secondSmallestAndSecondLargest(int[] arr)
+  {
+    if ( arr.length < 2)
+      return new int[]{-1, -1};
+    
+    int secondSmallest = Integer.MAX_VALUE;
+    int smallest = Integer.MAX_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
+    int largest = Integer.MIN_VALUE;
+    
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i] > largest)
+      {
+        secondLargest = largest;
+        largest = arr[i];
+      }
+      else if (arr[i] > secondLargest && arr[i] != largest)
+      {
+        secondLargest = arr[i];
+      }
+      
+      if (arr[i] < smallest)
+      {
+        secondSmallest = smallest;
+        smallest = arr[i];
+      }
+      else if (arr[i] < secondSmallest && arr[i] != smallest)
+      {
+        secondSmallest = arr[i];
+      }
+    }
+    return new int[]{secondSmallest, secondLargest};
+  }
+
+  // brute force
+  // O(n2)
+  public static boolean isSorted(int[] arr)
+  {
+    for (int i = 0; i < arr.length - 1; i++)
+    {
+      for (int j = i + 1; j < arr.length; j++)
+      {
+        if (arr[i] > arr[j])
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // optimal
+  // O(n)
+  public static boolean isSorted2(int[] arr)
+  {
+    for (int i = 0; i < arr.length -1; i++)
+    {
+      if (arr[i + 1] < arr[i])
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  // optimal
+  // O(n)
+  // brute force would be to use a set and loop through the array.
+  public static int removeDuplicates(int[] arr)
+  {
+    int i = 0;
+
+    if (arr.length == 1)
+    {
+      return 1;
+    }
+
+    for (int j = 1; j < arr.length; j++)
+    {
+      if (arr[i] != arr[j])
+      {
+        i++;
+        arr[i] = arr[j];
+      }
+      j++;
+    }
+    return i + 1;
+  }
+
+  // optimal
+  // O(n)
+  public static void leftRotateByOne(int[] arr)
+  {
+    int first = arr[0];
+
+    for (int i = 0; i < arr.length - 1; i++)
+    {
+      arr[i] = arr[i+1];
+    }
+    arr[arr.length-1] = first;
+  }
+
+
+  // Brute force
+  // O(n)
+  public static void rotateRightByKelements(int[] arr, int k)
+  {
+    if (arr.length == 0) return;
+    k = k % arr.length;
+
+    int[] tempArr = new int[k];
+    int j = 0;
+
+    for (int i = arr.length - k; i < arr.length; i++)
+    {
+      tempArr[j] = arr[i];
+      j++;
+    }
+
+    for (int i = arr.length - 1; i >= k; i--)
+    {
+      arr[i] = arr[i - k];
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+      arr[i] = tempArr[i];
+    } 
+  }
+
+  public static void rotateLeftByKelements(int[] arr, int k)
+  {
+    if (arr.length == 0) return;
+    k = k % arr.length;
+
+    int[] tempArr = new int[k];
+    int j = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+      tempArr[i] = arr[i];
+    }
+
+    for (int i = 0; i < arr.length - k; i++)
+    {
+      arr[i] = arr[i + k];
+    }
+
+    for (int i = arr.length - k; i < arr.length; i++)
+    {
+      arr[i] = tempArr[j];
+      j++;
+    }
+  }
+
+  public static void optimalRotateLeftByK(int[] arr, int k)
+  {
+    int n = arr.length;
+    k = k % n;
+
+    // reverse the first k elements
+    reverseArray(arr, 0, k-1);
+
+    // reverse the remaining n - k elements
+    reverseArray(arr, k, n - 1);
+
+    // reverse the entire array
+    reverseArray(arr, 0, n-1);
+  }
+
+  public static void optimalRotateRightByK(int[] arr, int k)
+  {
+    int n = arr.length;
+    k = k % n;
+
+    // reverse entire array
+    reverseArray(arr, 0, n - 1);
+
+    // reverse the first k elements
+    reverseArray(arr, 0, k-1);
+
+    // reverse the remaining n-k elements
+    reverseArray(arr, k, n-1);
+  }
+
+  private static void reverseArray(int[] arr, int start, int end)
+  {
+    while (start < end)
+    {
+      int temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  // not optimal
+  // O(n^2)
+  public static void moveAllZerosToEnd(int[] arr)
+  {
+    for (int i = arr.length-1; i > 0; i--)
+    {
+      int j = i-1;
+      // find a zero
+      while (j > 0 && arr[j] != 0)
+      {
+        j--;
+      }
+      if (arr[j] == 0)
+      {
+        // swap to right until we reach i
+        while (j < i)
+        {
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          j++;
+        }
+      } else {
+        return;
+      }
+    }
+  }
+
+  // O(n) time complexity
+  // O(1) space complexity
+  public static void optimalMoveAllZerosToEnd(int[] arr)
+  {
+    // find first zero
+    int j = -1;
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i] == 0)
+      {
+        j = i;
+        break;
+      }
+    }
+
+    // no zeros found
+    if (j == -1) return;
+    
+    for (int i = j+1; i < arr.length; i++)
+    {
+      if (arr[i] != 0)
+      {
+        // swap with arr[j]
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        // move j to next zero
+        j++;
+      }
+    }
+  }
+}
---
+++ src/test/java/ArrayzTest.java
@@ -0,0 +1,182 @@
+package test.java;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import main.java.Arrayz;
+
+public class ArrayzTest {
+  
+  @Test
+  public void test_largestElementInArray()
+  {
+    int[] input = {2, 5, 1, 3, 0};
+    int expected = 5;
+    int actual = Arrayz.largestElementInArray(input);
+
+    assertEquals(expected, actual);
+
+    actual = Arrayz.bruteforceLargestElementInArray(input);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void test_secondSmallestAndSecondLargest()
+  {
+    int[] input = {2, 5, 1, 3, 0};
+    int[] expected = {1, 3};
+    int[] actual = Arrayz.secondSmallestAndSecondLargest(input);
+    
+    assertArray(expected, actual);
+  }
+
+  @Test
+  public void test_secondSmallestAndSecondLargest_smallestArrayThatWorks()
+  {
+    int[] input = {2, 5, 1};
+    int[] expected = {2, 2};
+    int[] actual = Arrayz.secondSmallestAndSecondLargest(input);
+    
+    assertArray(expected, actual);
+  }
+
+  @Test
+  public void test_secondSmallestAndSecondLargest_arrayTooSmall()
+  {
+    int[] input = {2, 5};
+    int[] expected = {5, 2};
+    int[] actual = Arrayz.secondSmallestAndSecondLargest(input);
+    
+    assertArray(expected, actual);
+  }
+
+  @Test
+  public void test_isSorted()
+  {
+    int[] input = {1, 2, 4, 5, 10};
+    assertTrue(Arrayz.isSorted(input));
+
+    input = new int[]{1, 2, 3, 10, 4, 6, 5, 7 };
+    assertFalse(Arrayz.isSorted(input));
+
+    input = new int[]{1, 2, 3, 10, 4};
+    assertFalse(Arrayz.isSorted(input));
+
+    input = new int[]{2, 3, 4, 6, 1 };
+    assertFalse(Arrayz.isSorted(input));
+  }
+
+  @Test
+  public void test_isSorted2()
+  {
+    int[] input = {1, 2, 4, 5, 10};
+    assertTrue(Arrayz.isSorted2(input));
+
+    input = new int[]{1, 2, 3, 10, 4, 6, 5, 7 };
+    assertFalse(Arrayz.isSorted2(input));
+
+    input = new int[]{1, 2, 3, 10, 4};
+    assertFalse(Arrayz.isSorted2(input));
+
+    input = new int[]{2, 3, 4, 6, 1 };
+    assertFalse(Arrayz.isSorted2(input));
+  }
+
+  @Test
+  public void test_removeDuplicatesSorted()
+  {
+    int[] input = {1, 1, 2, 2, 2, 3, 3, 4, 5, 5};
+    int[] expectedArr = {1, 2, 3, 4, 5, 3, 3, 4, 5, 5};
+    int expectedReturn = 5;
+
+    int actualReturn = Arrayz.removeDuplicates(input);
+
+    assertEquals(expectedReturn, actualReturn);
+    assertArray(expectedArr, input);
+
+
+    input = new int[]{1, 1, 1, 1, 1};
+    expectedArr = new int[]{1, 1, 1, 1, 1};
+    expectedReturn = 1;
+
+    actualReturn = Arrayz.removeDuplicates(input);
+
+    assertEquals(expectedReturn, actualReturn);
+    assertArray(expectedArr, input);
+  }
+
+  @Test
+  public void test_leftRotateByOne()
+  {
+    int[] input = {1,2,3,4,5};
+    int[] expected = {2,3,4,5,1};
+
+    Arrayz.leftRotateByOne(input);
+    assertArray(expected, input);
+  }
+
+  @Test
+  public void test_rotateByKelements()
+  {
+    int[] input = {1,2,3,4,5,6,7};
+    int[] expected = {5,6,7,1,2,3,4};
+
+    Arrayz.rotateRightByKelements(input, 3);
+    assertArray(expected, input);
+
+    input = new int[]{1,2,3,4,5,6,7};
+    expected = new int[]{3,4,5,6,7,1,2};
+    Arrayz.rotateLeftByKelements(input, 2);
+    assertArray(expected, input);
+  }
+
+  @Test
+  public void test_optimalRotateLeftByK()
+  {
+    int[] input = {1,2,3,4,5};
+    int[] expected = {4,5,1,2,3};
+
+    Arrayz.optimalRotateLeftByK(input, 3);
+    assertArray(expected, input);
+  }
+
+  @Test
+  public void test_optimalRotateRightByK()
+  {
+    int[] input = {1,2,3,4,5};
+    int[] expected = {3,4,5,1,2};
+
+    Arrayz.optimalRotateRightByK(input, 3);
+    assertArray(expected, input);
+  }
+
+  @Test
+  public void test_moveAllZerosToEnd()
+  {
+    int[] input = {1,0,2,3,0,4,0,1};
+    int[] expected = {1,2,3,4,1,0,0,0};
+
+    Arrayz.moveAllZerosToEnd(input);
+    assertArray(expected, input);
+  }
+
+  @Test
+  public void test_optimalMoveAllZerosToEnd()
+  {
+    int[] input = {1,0,2,3,0,4,0,1};
+    int[] expected = {1,2,3,4,1,0,0,0};
+
+    Arrayz.optimalMoveAllZerosToEnd(input);
+    assertArray(expected, input);
+  }
+
+   
+  private void assertArray(int[] expected, int[] actual)
+  {
+    assertEquals(expected.length, actual.length);
+    for (int i = 0; i < expected.length; i++)
+    {
+      assertEquals(expected[i], actual[i]);
+    }
+  }
+
+}
